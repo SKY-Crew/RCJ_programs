7398f061fce3563bcb6d05f5071ae8065c04251f
SKY-Crew <43493783+SKY-Crew@users.noreply.github.com>
2019-05-04 09:33:40 +0900

    [Change] Change parameters

--- cal.ino
+++ cal.ino
@@ -1,19 +1,19 @@
 void get(data_t *d) {
 	d->gyro = Gyro.get();
-	d->goal = Cam.get();
+	d->goal = Cam.get(prvIsInAir);
 	Cam.send(double(d->gyro));
 
 	d->enemyStands[0] = frontPSD.getBool(false);
 	d->enemyStands[1] = backPSD.getBool(false);
 
-	const uint16_t THRE_BACK_PSD[2] = {950, 1300};
+	const uint16_t THRE_BACK_PSD[2] = {1200, 1500};
 	d->distGoalPSD = compare(backPSD.getVal(), THRE_BACK_PSD, 3, CLOSE);
 	if(isFW) {
-		const uint16_t THRE_DIST_FW[3] = {20, 34, 55};
+		const uint16_t THRE_DIST_FW[3] = {22, 37, 60};
 		d->distGoal = compare(d->goal.distOwn, THRE_DIST_FW, 4, TOO_CLOSE);
 	}else {
-		const uint16_t THRE_DIST_GK[2] = {13, 20};
-		Dist distGK = compare(d->goal.distOwn, THRE_DIST_GK, 3, PROPER);
+		const double THRE_DIST_GK[3] = {1.5, 4, 8};
+		Dist distGK = compare(d->goal.distOwn, THRE_DIST_GK, 4, CLOSE);
 		d->distGoal = Cam.getCanUse()
 				&& (backPSD.getVal() > 4000 || d->goal.diffOwn >= SMALL || distGK == TOO_FAR)
 				? distGK : d->distGoalPSD;
@@ -22,12 +22,14 @@ void get(data_t *d) {
 	d->ball = Ball.get();
 
 	d->distBall = compare(d->ball.r, THRE_DIST_BALL, 3, CLOSE);
-	d->isBallForward = d->distBall == CLOSE && d->ball.t.isUp(15) && Ball.getForward() >= 540;
+	d->isBallForward = d->distBall == CLOSE && d->ball.t.isUp(15) && Ball.getForward() >= 450;
 	d->catchingBall = Ball.getCatch() && d->ball.t.isUp(30) && d->distBall == CLOSE;
 	d->catchFreely = d->catchingBall && !d->enemyStands[0]
 			&& (isFW || d->distGoal >= FAR || !Cam.getCanUse());
 
 	d->line = Line.get(isFW, d->gyro, Gyro.getDiff(), d->goal.isInCorner != 0);
+	prvIsInAir = d->line.isInAir;
+	d->line.isOutside |= !bool(d->ball.t) && bool(d->line.dirInside);
 
 	d->fellow = Comc.rcv(isFW);
 	Comc.snd(canRun, isFW, d->ball.r, d->goal.distOwn, d->distGoal <= CLOSE, d->line.isInAir);
@@ -40,13 +42,13 @@ Angle calDir(bool isFW, vectorRT_t ball, double distGoal) {
 	if(isFW) {
 		dir = Ball.getDir(ball);
 	}else {
-		dir = constrain(map(distGoal, 5, 60, 90, 180), 90, 180); ////
+		dir = constrain(map(distGoal, 5, 60, 90, 180), 85, 180);
 	}
 	trace(11) { Serial.println("dir:"+str(dir)); }
 	return dir;
 }
 
-int16_t calRot(bool isFW, cam_t goal, Angle gyro, bool catchingBall, bool isBallForward) {
+int16_t calRot(bool isFW, cam_t goal, Angle gyro, bool catchingBall, bool isBallForward, bool isOnLine) {
 	int16_t rot = 0;
 	if(isFW) {
 		if(Cam.getCanUse() && !bool(gyro)) {
@@ -54,10 +56,10 @@ int16_t calRot(bool isFW, cam_t goal, Angle gyro, bool catchingBall, bool isBall
 			rot = Cam.multiRotGoal(goal.rotOpp);
 		}else if(bool(gyro)) {
 			// 両方 or gyroのみ
-			rot = Gyro.multiRot(0);
+			rot = Gyro.multiRot(0, isOnLine);
 		}
 	}else {
-		rot = Gyro.multiRot(0);
+		rot = Gyro.multiRot(0, isOnLine);
 	}
 	trace(12) { Serial.println("rot:"+str(rot)); }
 	return rot;
@@ -90,71 +92,89 @@ void checkRole(bool canBecomeGK, bool onGround, comc_t fellow, double ball_r, do
 	}
 }
 
-void avoidMulDef(Angle *dir, comc_t fellow, vectorRT_t ball, Dist distGoal) {
-	if(fellow.exists || true) {
-		if(ball.t.isDown(90)) {
+bool avoidMulDef(Angle *dir, comc_t fellow, vectorRT_t ball, Dist distGoal, cam_t goal) {
+	if(digitalRead(57)) { // fellow.exists && Comc.getCanUse()) {
+		if(ball.t.isDown(100)) {
 			switch (distGoal) {
 			// 少し後ろ
 			case CLOSE:
-				*dir = ball.t.isDown(10) ? Angle(false)
-						: 80 * signum(ball.t);
+				*dir = ball.t.isDown(15) ? Angle(false)
+						: 90 * signum(ball.t);
+				return true;
 				break;
 			// 後ろ過ぎ
 			case TOO_CLOSE:
-				*dir = ball.t.isDown(10) ? 0
-						: 50 * signum(ball.t);
+				*dir = ball.t.isDown(15) || !bool(ball.t) ? 0
+						: 70 * signum(ball.t);
+				return true;
 				break;
 			default:
 				break;
 			}
+			if(goal.diffOwn >= TOO_LARGE && bool(goal.rotOwn)) {
+				*dir = false;
+				return true;
+			}
 		}
 	}
+	return false;
 }
 
-void detectEnemyBack(Angle *dir, vectorRT_t ball, bool enemyStandsBack) {
+bool detectEnemyBack(Angle *dir, vectorRT_t ball, bool enemyStandsBack) {
 	if(enemyStandsBack && dir->isDown(70)) {
 		*dir = constrainAngle(ball.t, -90, 90);
+		return true;
 	}
+	return false;
 }
 
-void detectBallOutside(Angle *dir, line_t line, Angle gyro) {
+bool detectBallOutside(Angle *dir, line_t line, Angle gyro) {
 	if(bool(line.dirInside) && bool(*dir)) {
 		Angle absoluteDI = line.dirInside - gyro;
 		if(absoluteDI.isRight(45)) {
-			if(line.canPause && (*dir - absoluteDI).inside(180 - 10, 180 + 20)) {
+			if(line.canPause && (*dir - absoluteDI).inside(180 - 30, 180 + 30)) {
 				// 停止
 				*dir = Angle(false);
+				return true;
 			}else if((*dir - absoluteDI).inside(90, 180)) {
 				// 後退
 				*dir = absoluteDI + 90;
+				return true;
 			}
 		}else if(absoluteDI.isLeft(45)) {
-			if(line.canPause && (*dir - absoluteDI).inside(180 - 20, 180 + 10)) {
+			if(line.canPause && (*dir - absoluteDI).inside(180 - 30, 180 + 30)) {
 				// 停止
 				*dir = Angle(false);
+				return true;
 			}else if((*dir - absoluteDI).inside(-180, -90)) {
 				// 後退
 				*dir = absoluteDI - 90;
+				return true;
 			}
 		}
 	}
+	return false;
 }
 
-void detectLineForward(Angle *dir, vectorRT_t ball, Dist distBall) {
-	bool isBallOutside = ball.t.isUp(bool(cLineForward) ? 25 : 35);
+bool detectLineForward(Angle *dir, vectorRT_t ball, Dist distBall, Angle gyro) {
+	Angle absoluteBall_t = ball.t - gyro;
+	bool isBallOutside = absoluteBall_t.isUp(bool(cLineForward) ? 25 : 35);
 	switch(sideLF) {
-		case LEFT: isBallOutside |= ball.t.inside(-60, 0); break;
-		case RIGHT: isBallOutside |= ball.t.inside(0, 60); break;
+		case LEFT: isBallOutside |= absoluteBall_t.inside(-60, 0); break;
+		case RIGHT: isBallOutside |= absoluteBall_t.inside(0, 60); break;
 		default: break;
 	}
 
 	if(bool(cLineForward)) {
 		*dir = false;
 		cLineForward.increase(distBall >= PROPER && isBallOutside);
+		return true;
 	}else if(cLineForward.compare(0)) {
-		*dir = 180;
+		*dir = 180 + gyro;
 		cLineForward.increase(isBallOutside);
+		return true;
 	}else {
 		cLineForward.increase(false);
 	}
+	return false;
 }
--- move.ino
+++ move.ino
@@ -40,17 +40,20 @@ void correctRot(bool isFW, Angle gyro) {
 	}
 }
 
-void carryBall(bool isFW, int16_t rot, cam_t goal, Angle gyro, bool catchingBall, bool enemyStandsFront) {
+void carryBall(bool isFW, int16_t rot, cam_t goal, Angle gyro, bool catchingBall, bool enemyStandsFront, bool leavingLine, bool isBallForward) {
 	willCarryBall = carryingBall;
 	if(carryingBall) {
 		cLineForward.increase(false);
+		uint8_t powerCB = isBallForward ? 200 : 230; // constrain(150 + map(millis() - timeStartCB, 0, 200, 0, 50), 150, 200);
 		if(millis() - timeStartCB < 1500) {
-			if(isFW) {
+			if(isFW && Cam.getCanUse() && bool(gyro)) {
 				if(enemyStandsFront) {
-					Motor.run(bool(gyro) ? gyro * (-0.5) : 0, Gyro.multiRot(goal.rotOpp), 180); ////
+					Motor.run(gyro * (-0.5), Gyro.multiRot(signum(goal.rotOpp) * 45, false), powerCB);
+				}else {
+					Motor.run(leavingLine ? goal.rotOpp : 0, Cam.multiRotGoal(goal.rotOpp), powerCB);
 				}
 			}else {
-				Motor.run(0, rot, 170);
+				Motor.run(0, rot, powerCB);
 			}
 		}else {
 			// スタック
@@ -70,6 +73,7 @@ void carryBall(bool isFW, int16_t rot, cam_t goal, Angle gyro, bool catchingBall
 void ballInAir(bool isBallInAir, int16_t rot, Angle rotOwn, Dist distGoal, vectorRT_t ball) {
 	if(isBallInAir && Cam.getCanUse()) {
 		// ボール真上前方・ボールない
+		cLineForward.reset();
 		if(distGoal >= FAR) {
 			Motor.run(180, rot, 190);
 		}else if(distGoal > CLOSE) {
@@ -80,6 +84,7 @@ void ballInAir(bool isBallInAir, int16_t rot, Angle rotOwn, Dist distGoal, vecto
 			Motor.run(distGoal < CLOSE ? 0 : Angle(false), rot, 100);
 		}
 	}else if(isBallInAir && ball.t.isUp(60)) {
+		cLineForward.reset();
 		Motor.run(false, rot, 0);
 	}
 }
@@ -90,23 +95,26 @@ void run(data_t *d, bool isFW, Angle dir, int16_t rot) {
 		// FW
 		bool leavingLine = bool(d->line.dirInside) && abs(dir - d->line.dirInside) <= 90;
 		carryBall(isFW, rot, d->goal, d->gyro,
-				d->catchingBall || d->isBallForward, d->enemyStands[0]);
-		ballInAir(Ball.getIsInAir() || !bool(d->ball.t),
+				d->catchingBall || d->isBallForward, d->enemyStands[0], leavingLine, d->isBallForward);
+		ballInAir(!bool(d->ball.t),
 				rot, d->goal.rotOwn, d->distGoal, d->ball);
+		if(cLineForward.compare(0)) {
+			Motor.run(dir, Gyro.multiRot(0, bool(d->line.dirInside)), 140);
+		}
 		if(d->ball.t.isRight(45) || d->ball.t.isLeft(45)) {
 			//ボール横
-			Motor.run(dir, rot, (leavingLine || d->distGoal == CLOSE) ? 140 : 190);
+			Motor.run(dir, rot, (leavingLine || d->distGoal == CLOSE) ? 110 : 140);
 		}else if(d->ball.t.isDown(45) || d->distBall >= FAR) {
 			// ボール(後方|遠く)
-			Motor.run(dir, rot, (leavingLine || d->distGoal == CLOSE) ? 130 : 160);
+			Motor.run(dir, rot, (leavingLine || d->distGoal == CLOSE) ? 160 : 190);
 		}else {
 			// ボール前方
-			Motor.run(dir, rot, leavingLine ? 100 : 130);
+			Motor.run(dir, rot, leavingLine ? 90 : 120);
 		}
 		Kicker.run(d->catchFreely && d->goal.isOppWide);
 	}else {
 		// GK
-		d->isBallForward |= d->ball.t.isUp(10) && d->distBall == CLOSE;
+		d->isBallForward |= d->ball.t.isUp(d->distBall == CLOSE ? 10 : 3) && d->distBall <= PROPER;
 		if(d->goal.diffOwn == TOO_LARGE
 				|| (d->goal.diffOwn == LARGE && d->distBall > CLOSE)) {
 			// 横行きすぎ・横&ボール遠く
@@ -119,28 +127,33 @@ void run(data_t *d, bool isFW, Angle dir, int16_t rot) {
 			Motor.run(Ball.getDir(d->ball), rot, 200);
 		}
 		// ボール捕獲
-		carryBall(isFW, rot, d->goal, d->gyro, d->catchingBall, false);
+		carryBall(isFW, rot, d->goal, d->gyro, d->catchingBall, false, false, false);
 		if(d->isBallForward) {
 			// ボール前方直線上
 			Motor.run(0, rot, 150);
-		}else if(d->ball.t.isDown(80) && d->distGoal <= PROPER) {
+		}else if(d->ball.t.isDown(45) && d->distGoal <= PROPER) {
 			// ボール後方&ゴール遠くない
 			Motor.run(false, rot, 0);
-		}else if(d->goal.diffOwn == SMALL && d->distBall <= CLOSE) {
+		}else if(d->goal.diffOwn == SMALL && d->distBall <= CLOSE && d->goal.sideOwn * signum(d->ball.t) == 1) {
 			// 少し横&ボール近く
 			Motor.run(dir * signum(d->ball.t), rot, 90);
 		}else if(!bool(d->ball.t)
-				|| d->ball.t.isUp(5)
+				|| d->ball.t.isUp(d->distBall <= PROPER ? 5 : 3)
 				|| d->goal.sideOwn * signum(d->ball.t) == 1) {
 			// ボールない(ゴール遠くない)・ボール前方(遠く)・ボール外側(自分端)
 			Motor.run(d->distGoal == CLOSE ? 0 : d->distGoal >= FAR ? 180 : Angle(false),
-					rot, 60);
-		}else if(d->ball.t.isDown(80) && d->distBall <= CLOSE){
+					rot, 70);
+		}else if(d->ball.t.isDown(45) && d->distBall <= CLOSE){
 			//ボール後方近く(ゴール遠い)
 			Motor.run(Ball.getDir(d->ball), rot, 140);
+		}else if(d->distBall >= FAR) {
+			Motor.run(dir * signum(d->ball.t), rot,
+					constrain(map(double(abs(d->ball.t)), 3, 15, 30, 170), 0, 200));
 		}else {
-			//ボール(前方|遠い)(ゴール遠い)
-			Motor.run(dir * signum(d->ball.t), rot, 140);
+			Motor.run(dir * signum(d->ball.t), rot,
+					d->ball.t.isUp(20) ?
+							constrain(map(double(abs(d->ball.t)), 5, 20, 30, 100), 0, 100)
+					: d->ball.t.isUp(35) ? 170 : 240);
 		}
 		Kicker.run(d->catchFreely && !d->fellow.exists);
 	}
